5e01512d2e71cbbe4761770ef22fd1b5876d9cda
Andrul01 <andrumorallies@gmail.com>
2025-10-10 00:42:33 +0530

    Initialize React application with Vite, ESLint, and basic structure
    - Created package.json with dependencies for React, Vite, and ESLint.
    - Added Vite logo SVG to public directory.
    - Created App.css for styling with a test class.
    - Developed App component with a Hello component and React logo.
    - Implemented Hello component displaying a greeting message.
    - Added React logo SVG to assets directory.
    - Established global styles in index.css for consistent theming.
    - Set up main entry point in main.jsx for rendering the App component.
    - Configured Vite with a basic setup in vite.config.js.

---
+++ my-app/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Hello from './Hello'
+import reactLogo from './assets/react.svg'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Andrul</h1>
+        <Hello />
+      </div>
+      <div>
+        <img src={reactLogo} width="200px" />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ my-app/src/Hello.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+const Hello = () => {
+     const name = "Andrul" 
+  return (
+    <div className='test'>Hello {name} gaming</div>
+  )
+}
+
+export default Hello
