8f0fe7e4e1d5a3dafaa82ecb61a96c0969faee4b
Andrul01 <andrumorallies@gmail.com>
2025-10-12 13:59:13 +0530

    feat: enhance App component with additional functionality and styling

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,13 +3,78 @@ import Hello from './Hello'
 import reactLogo from './assets/react.svg'
 
 function App() {
+  const headingStyle = {
+    color: 'blue',
+    backgroundColor: 'lightgray',
+    padding: '10px',
+    borderRadius: '5px',
+    textAlign: 'center'
+  }
+  
+  //classic function
+  function getName1(gameName){
+    return gameName;
+  }
 
+  //arrow function
+  const getName2 = (gameName) => {
+    return gameName;
+  }
+  const title = "Top Trending Games 2024"
+  const game1 = "God of War"
+  const game2 = "Wwe 2k24"
+  const game3 = "Fifa 24"
+  const game4 = "Grand Theft Auto V"
+
+  function handleClick(){
+    alert("Button Clicked")
+  }
+
+  const handleInput = (event) => {
+    console.log("Input Changed:", event.target.value)
+  }
+
+  const handleMouseOver = () => {
+    console.log("Mouse Over Event Triggered")
+  }
+
+  const handleDoubleClick = () => {
+    console.log("Paragraph Double Clicked")
+  }
+  
   return (
     <>
       <div>
-        <h1>Andrul</h1>
+        <h1 style={headingStyle} >Andrul</h1>
         <Hello />
       </div>
+      <div>
+        <h1>{getName1(title)}</h1>
+        <h2>{getName1(game1)}</h2>
+        <h2>{getName1(game2)}</h2>
+        <h2>{getName1(game3)}</h2>
+        <h2>{getName1(game4)}</h2>
+      </div>
+      <p onMouseOver={handleMouseOver} onDoubleClick={handleDoubleClick}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, quaerat!</p>
+      <div >
+        <button onClick={handleClick}>Click Me</button>
+      </div>
+      <div >
+        <button onClick={() => alert("hello andrul")}>Say Hello</button>
+      </div>
+      <div>
+        <input type="text" onChange={handleInput} placeholder='Enter your text'/>
+      </div>
+
+
+      <div>
+        <h1>{getName2(title)}</h1>
+        <h2>{getName2(game1)}</h2>
+        <h2>{getName2(game2)}</h2>
+        <h2>{getName2(game3)}</h2>
+        <h2>{getName2(game4)}</h2>
+      </div>
+      
       <div>
         <img src={reactLogo} width="200px" />
       </div>
--- my-app/src/Hello.jsx
+++ my-app/src/Hello.jsx
@@ -1,9 +1,12 @@
-import './App.css'
+// import './App.css'
+import styles from "./Hello.module.css"
 
 const Hello = () => {
      const name = "Andrul" 
+     
   return (
-    <div className='test'>Hello {name} gaming</div>
+    // <div className='test'>Hello {name} gaming</div>
+    <div className={styles.test}>Hello {name} gaming</div>
   )
 }
 
